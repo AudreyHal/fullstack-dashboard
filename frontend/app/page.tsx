9291f6edc5144da19797f95f07de79529c877723
AudreyHal <chiomahalim@gmail.com>
2024-07-19 13:14:24 +0100

    Initial commit

---
+++ frontend/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import TopBar from "./components/TopBar";
+import SummaryCard from "./components/SummaryCard";
+
+export default function Home() {
+  return (
+    <div className="w-full h-full  m-10 font-semibold p-8 ">
+
+    </div>
+  );
+}
