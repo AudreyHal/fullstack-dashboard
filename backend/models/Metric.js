b573e1d4b27d3368140eacfc1bcc66b941774a4e
AudreyHal <chiomahalim@gmail.com>
2024-07-23 14:28:19 +0100

    Add models

---
+++ backend/models/Metric.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const metricSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  value: { type: Number, required: true },
+  timestamp: { type: Date, required: true, default: Date.now },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+});
+
+const Metric = mongoose.model('Metric', metricSchema);
+
+module.exports = Metric;
