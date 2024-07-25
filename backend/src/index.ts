fd9cf5c0394107874685865e704d15e8462ea9c3
AudreyHal <chiomahalim@gmail.com>
2024-07-25 18:42:33 +0100

    Load dotenv

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,11 @@ import registerRoutes from './routes/register';
 import loginRoutes from './routes/login';
 import metricRoutes from './routes/metrics';
 import authenticate  from './middleware/authenticate';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+const mongoUri = process.env.ATLAS_URI || '';
 
 const app: Express = express();
 const port: number = parseInt(process.env.PORT || '3002', 10);
@@ -19,7 +24,7 @@ app.use('/api/login', loginRoutes);
 app.use('/api/metrics', authenticate, metricRoutes);
 
 // Connects to MongoDB and starts the server
-mongoose.connect('mongodb+srv://Mongo:Mongo123@metricscluster.yaqpfk6.mongodb.net/Metrics-API?retryWrites=true&w=majority&appName=MetricsCluster'
+mongoose.connect(mongoUri
 ).then(() => {
   console.log("Connected to MongoDB");
   app.listen(port, () => {
